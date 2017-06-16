ed018c1c8233fc69f35ac6171bf8c3cab5bae8d7
Spencer Rohan <spencermrohan@gmail.com>
2017-06-16 15:20:43 -0500

    ADD brainstormed skeleton functions with notes

---
+++ unitas.js
@@ -0,0 +1,27 @@
+function foo1(num){
+	//Equal
+}
+
+function foo2(num){
+	//Switch Case
+}
+
+function foo3(num){
+	//Greater than or less than
+}
+
+function foo4(num){
+	//Array
+}
+
+function foo5(num){
+	//Hash
+}
+
+function foo6(num){
+	//Not Operator
+}
+
+function foo7(num){
+	//Ternary based on assumptions
+}
